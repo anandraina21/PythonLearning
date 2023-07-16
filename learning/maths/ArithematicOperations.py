1c1cd99bc0e8bf944fcedcd92cc2380beaf41201
Anand Raina <anandraina95@gmail.com>
2023-07-16 16:08:36 +0530

    Arithematic Operations and formatted folder structure

---
+++ learning/maths/ArithematicOperations.py
@@ -0,0 +1,16 @@
+print('Addition of 10 + 3 :', 10 + 3)
+print('Subtraction of 10 - 3 :', 10 - 3)
+print('Multiplication of 10 * 3 :', 10 * 3)
+print('Division(float) of 10 / 3 :', 10 / 3)
+print('Division(int) of 10 // 3 :', 10 // 3)
+print('Modulus of 10 % 3 :', 10 % 3)
+print('10 to the power of 3 :', 10 ** 3)
+
+x = 1
+x = x + 2
+x += 3
+print(x)
+
+# Python uses PEMDAS for operator precedence
+y = 10 + 3 / 3 * (5 * 2)
+print(y)
