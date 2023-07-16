4d47c42431bb38fb9d9d5f139bfaf9c8445d3b7b
Anand Raina <anandraina95@gmail.com>
2023-07-16 15:39:07 +0530

    Formatted Strings

--- learning/FormattedStrings.py
+++ learning/FormattedStrings.py
@@ -2,6 +2,9 @@ first = 'Anand'
 last = 'Raina'
 message = first + ' [' + last + '] is a coder.'
 print(message)
+# Example of formatted string (string with placeholders)
+formatted_str = f'{first} {last} is a cool guy!'
+print(formatted_str)
 
 message2 = first, last, 'Cool', "Bye", "\"Abc\""
 print(message2)
