bf65cbb23ec3f78169fe92c9f5c742258d927115
Anand Raina <anandraina95@gmail.com>
2023-07-16 15:54:33 +0530

    String methods

--- learning/StringMethods.py
+++ learning/StringMethods.py
@@ -1,1 +1,16 @@
 course = 'Python for Beginners'
+print(len(course))
+
+print(course.upper())
+print(course.lower())
+
+# Returns the first index of the item to be searched
+print(course.find('o'))
+print(course.find('or'))
+print(course.find('1'))
+
+print(course.replace('Python', 'Programming'))
+print('Python' in course)
+# To return string with first character in a word in uppercase and the others in lowercase
+course1 = 'AnAnd raina'
+print(course1.title())
